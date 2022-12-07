279cb8c1ee1c98dd769c0b6959accd8b6ff14a26
Didier Osindero <didierosindero@gmail.com>
2022-12-07 09:01:17 +0000

    Create CLI to search for movies

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -6,3 +6,20 @@ import { Client } from "pg";
 //we only need to specify the (non-default) database name.
 const client = new Client({ database: 'omdb' });
 console.log("Welcome to search-movies-cli!");
+
+const myCLI = async () => {
+    await client.connect();
+    let searchInput: string;
+
+    while (true) {
+        searchInput = await question('Type in a film name: \n')
+        if (searchInput === 'q') {
+            break;
+        }
+        const queryResponse = await client.query(`SELECT * FROM movies WHERE lower(name) LIKE '%${searchInput.toLowerCase()}%'`);
+        console.log(`RESULTS: \n`)
+        console.table(queryResponse.rows);
+    }
+    await client.end();
+} 
+myCLI();
